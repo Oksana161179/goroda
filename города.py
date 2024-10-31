335b383ab909a3240169f0f0b872921e8538ee65
Оксана <oksana.fomenko.161979@gmail.com>
2024-10-31 20:45:08 +0600

    Добавлено округление координат

--- города.py
+++ города.py
@@ -8,14 +8,18 @@ def get_coordinates(city, key):#создаем функцию получения
         geocoder = OpenCageGeocode(key)
         results = geocoder.geocode(city, language='ru')
         if results:#делаем проверку:если все хорошо, возвращает нам информацию
-            return results[0]['geometry']['lat'], results[0]['geometry']['lng']#возвращаем долготу и широту
+            lat = round(results[0]['geometry']['lat'], 2)#создаем переменную для широты
+            # и округляем ее до двух знаков после запятой
+            lon = round(results[0]['geometry']['lng'], 2)#создаем переменную для долготы
+            # и округляем ее до двух знаков после запятой
+            return lat, lon#возвращаем широту и долготу
         else:#если же ничего нет
             return "Город не найден"#то возвращается сообщение об этом
     except Exception as e:#обрабатываем исключения
         return f"Возникла ошибка: {e}"#возвращается сообщение об ошибке
 
 key = 'fb6b2a8e60d74ae397a69757e0b1f27b'
-city = "London"
+city = "Химки"
 coordinates = get_coordinates(city, key)#создаем переменную-coordinates, в которую положим вызов функции.
 # а передаем в ней город и ключ
 print(f"Координаты города {city}: {coordinates}")#выводим в консоль название города-{city}
