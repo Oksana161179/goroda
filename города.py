c363facf1e1faecc38e15231a2f6a9612c5577b4
Оксана <oksana.fomenko.161979@gmail.com>
2024-11-01 16:47:14 +0600

    Добавлена возможность отображения карты города в браузере при нажатии на кнопку.

--- города.py
+++ города.py
@@ -1,6 +1,6 @@
-from future.backports import count
 from opencage.geocoder import OpenCageGeocode
 from tkinter import *
+import webbrowser
 
 def get_coordinates(city, key):#создаем функцию получения координат:
     # передаем город(название) и ключ
@@ -13,29 +13,44 @@ def get_coordinates(city, key):#создаем функцию получения
             lon = round(results[0]['geometry']['lng'], 2)#создаем переменную для долготы
             # и округляем ее до двух знаков после запятой
             country = results[0]['components']['country']#выводим результаты о стране, в котором находится город
+            osm_url = f"https://www.openstreetmap.org/?mlat={lat}&mlot={lon}"#отправляем запрос на сайт карт
 
             if 'state' in results[0]['components']:#делаем проверку: проверяем, существует ли регион, если да
                 region = results[0]['components']['state']#выводим результаты о регионе, в котором находится город
-                return f"Широта: {lat}, Долгота: {lon}\n Страна: {country}.\n Регион: {region}"
+                return {
+                    "coordinates": f"Широта: {lat}, Долгота: {lon}\n Страна: {country}.\n Регион: {region}",
+                    "map_url": osm_url
+                        }
+
                 #возвращаем широту, долготу, страну и регион
             else:#если же не существует
-                return f"Широта: {lat}, Долгота: {lon}\n Страна: {country}"#то выводим без региона
+                return {
+                    "coordinates": f"Широта: {lat}, Долгота: {lon}\n Страна: {country}.",
+                    "map_url": osm_url
+                        }
         else:#если же ничего нет
             return "Город не найден"#то возвращается сообщение об этом
     except Exception as e:#обрабатываем исключения
          return f"Возникла ошибка: {e}"#возвращается сообщение об ошибке
 
 def show_coordinates(event=None):#создаем функцию, которая будет вызываться после нажатия на кнопку
+    global map_url
     city = entry.get()#получаем название города из поля ввода
-    coordinates = get_coordinates(city, key)#создаем переменную-coordinates, в которую положим вызов функции.
+    result = get_coordinates(city, key)#создаем переменную-coordinates, в которую положим вызов функции.
     # а передаем в ней город и ключ
-    label.config(text=f"Координаты города {city}:\n {coordinates}")#конфигурируем кнопку
+    label.config(text=f"Координаты города {city}:\n {result['coordinates']}")#конфигурируем кнопку
+    map_url = result['map_url']
+
+def show_map():#создаем функцию показа города на карте, которая привязана к кнопке
+    if map_url:#и если карта существует, если этот путь не пустой
+        webbrowser.open(map_url)# тогда мы открываем карту
 
 key = 'fb6b2a8e60d74ae397a69757e0b1f27b'
+map_url = ""
 
 window = Tk()#создаем окно
 window.title("Координаты городов")#задаем заголовок окну
-window.geometry("320x115")#задаем размер окну
+window.geometry("320x160")#задаем размер окну
 
 entry = Entry()#создаем поле ввода
 entry.pack()
@@ -47,6 +62,10 @@ button.pack()
 
 label = Label(text="Введите город и нажмите на кнопку")#создаем метку
 label.pack()
+
+map_button = Button(text="Показать карту", command=show_map)#создаем кнопку
+map_button.pack()
+
 
 
 window.mainloop()
